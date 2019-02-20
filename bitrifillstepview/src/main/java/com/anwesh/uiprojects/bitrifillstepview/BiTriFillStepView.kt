b7e50e63f050c12c55eb913bba38cc086f867183
Anwesh43 <anweshthecool0@gmail.com>
2019-02-20 10:29:03 +0530

    adding drawBTFNode canvas extension method

--- bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
+++ bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
@@ -8,10 +8,7 @@ import android.view.View
 import android.view.MotionEvent
 import android.content.Context
 import android.app.Activity
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Color
-import android.graphics.Path
+import android.graphics.*
 
 val nodes : Int = 5
 val tris : Int = 2
@@ -27,4 +24,42 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * this
+
+fun Canvas.drawTriangle(size : Float, sc :Float, paint : Paint) {
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(size, 0f)
+    path.lineTo(0f, -size)
+    path.lineTo(0f, 0f)
+    paint.style = Paint.Style.STROKE
+    drawPath(path, paint)
+    paint.style = Paint.Style.FILL
+    val cp = Path()
+    cp.addRect(RectF(0f, -size, size * sc, 0f), Path.Direction.CW)
+    clipPath(cp)
+    drawPath(path, paint)
+}
+
+fun Canvas.drawBTFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    for (j in 0..(tris - 1)) {
+        save()
+        scale(j.sf(), j.sf())
+        drawTriangle(size, sc1.divideScale(j, 2), paint)
+        restore()
+    }
+    restore()
+}
