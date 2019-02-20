ccad51b0c1f4e992d31e769537f280d2e55071f6
Anwesh43 <anweshthecool0@gmail.com>
2019-02-20 10:42:21 +0530

    adding BTFNode class

--- bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
+++ bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
@@ -128,4 +128,48 @@ class BiTriFillStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTFNode(var i : Int, val state : State = State()) {
+
+        private var next : BTFNode? = null
+        private var prev : BTFNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BTFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas  : Canvas, paint : Paint) {
+            canvas.drawBTFNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTFNode {
+            var curr : BTFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
