7f9053081057210992998a17ffab1a8f73b643c3
Anwesh43 <anweshthecool0@gmail.com>
2019-02-20 10:44:51 +0530

    adding BiTriFillStep class

--- bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
+++ bitrifillstepview/src/main/java/com/anwesh/uiprojects/bitrifillstepview/BiTriFillStepView.kt
@@ -172,4 +172,28 @@ class BiTriFillStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiTriFillStep(var i : Int) {
+
+        private val root : BTFNode = BTFNode(0)
+        private var curr : BTFNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
